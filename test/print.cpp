533313f9041f697cf296aa0b649956e28f1d9657
Logan Leland <leland.6@wright.edu>
2022-06-22 22:01:52 -0400

    add GCC test

---
+++ test/print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int a(0);
+  std::cin >> a;
+  if(a > 25) {
+    std::cout << "With peace and love, parse.\n";
+  }
+  std::cout << "Hello World!\n";
+  return 0;
+}
